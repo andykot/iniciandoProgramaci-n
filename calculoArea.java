4e4f5c16963b6191a6767f506c2eda5342572da8
andykot <48845906+andykot@users.noreply.github.com>
2019-03-26 22:26:47 -0300

    Update calculoArea.java

--- calculoArea.java
+++ calculoArea.java
@@ -5,9 +5,9 @@ public class calculoArea {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-        float calc=0;
+        float calc;
 		Scanner geometria = new Scanner(System.in);
-		 System.out.println("¿ escribael numero que acompaña a la  figura quiere calcularle el área");
+		 System.out.println("Â¿ escriba el numero que acompaÃ±a a la  figura que le quiere calcularle el Ã¡rea");
 		 System.out.println("1_cuadrado");
 		 System.out.println("2_rectangulo");
 		 System.out.println("3_triangulo");
@@ -19,7 +19,7 @@ public class calculoArea {
 		 
 		 case (1):
 			 
-			 String lado = JOptionPane.showInputDialog("valor del lado");
+		    String lado = JOptionPane.showInputDialog("valor del lado");
 		     
 		    float lado1=Float.parseFloat(lado);
 		   
@@ -29,19 +29,19 @@ public class calculoArea {
 		   case(2):
 		   //voy a usar metodo simplificado del caso 1
 			   
-			float ladoCor=Float.parseFloat(JOptionPane.showInputDialog("Introduce valor lado corto"));
+		   float ladoCor=Float.parseFloat(JOptionPane.showInputDialog("Introduce valor lado corto"));
 		   float ladoLar=Float.parseFloat(JOptionPane.showInputDialog("Introduce lado largo"));
 			
 		 
 		   calc=(ladoCor*ladoLar);
 		   break;
-		     
-		 default: System.out.println("el cálculo del áre del triangulo o circulo es para version premium, revisa que no hayas marcado valor invalido");
+		    
+		 //me da fastidio con la opciÃ³n triangulo y circulo salgo del case 
+		 default: System.out.println("el cÃ¡lculo del Ã¡re del triangulo o circulo es solo para version premium, revisa que no hayas marcado valor invalido");
 		 		
 		 	 
 		 }
 		 
-		 //me da ladilla seguir con triangulo y circulo salgo del case
 		 
 		 if(figura==1 || figura==2) {
 		System.out.println("el valor del area es "+ calc + " unidades");
